ff23711ceb6378f58b9be10d34856e1a89976517
RhysCrowell <157551589+RhysCrowell@users.noreply.github.com>
2024-04-21 18:30:50 -0700

    Work Done for the Code Review Assignmnet

---
+++ Rhys/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct User {
+    string username;
+    string displayName;
+    string state;
+    vector<string> friends;
+};
+
+struct Post {
+    string postId;
+    string userId;
+    string visibility;
+};
+
+vector<User> users;
+vector<Post> posts;
+
+void loadUserData(const string& filename){
+    ifstream file(filename);
+    string entryLine;
+    while(getline(file, entryLine)){
+        stringstream ss(entryLine);
+        User user;
+        string friends;
+        getline(ss, user.username, ';');
+        getline(ss, user.displayName, ';');
+        getline(ss, user.state, ';');
+        getline(ss, friends);
+        friends = friends.substr(1, friends.length()-2);
+        stringstream fs(friends);
+        string friendName;
+        while(getline(fs,friends,',')){
+            user.friends.push_back(friendName);
+        }
+        users.push_back(user);
+    }
+}
+
+void loadPostData(const string& filename){
+    ifstream file(filename);
+    string entryLine;
+    while(getline(file, entryLine)){
+        stringstream ss(entryLine);
+        Post post;
+        getline(ss, post.postId, ';');
+        getline(ss, post.userId, ';');
+        getline(ss, post.visibility);
+        posts.push_back(post);
+    }
+}
+
+string checkVisibility(const string& postId, const string& username){
+    for(Post post : posts){
+        if(post.postId == postId){
+            if(post.visibility == "public"){
+                return "Access Permitted";
+            }
+            for(User user : users){
+                if(user.username == post.userId){
+                    if(find(user.friends.begin(), user.friends.end(), username) != user.friends.end()){
+                        return "Access Permitted";
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return "Access Denied";
+}
+vector<string> retrievePosts(const string& username){
+    vector<string> visiblePosts;
+    for(Post post : posts){
+        if(post.userId != username){
+            if(post.visibility == "public"){
+                visiblePosts.push_back(post.postId);
+            }else{
+                const auto& userIt = find_if(users.begin(), users.end(), [&post](const User& u) { return u.username == post.userId; });
+                if (userIt != users.end() && find(userIt->friends.begin(), userIt->friends.end(), username) != userIt->friends.end()) {
+                    visiblePosts.push_back(post.postId);
+                }
+            }
+        }
+    }
+    return visiblePosts;
+}
+vector<string> searchUsersByLocation(const string& state){
+    vector<string> matchedUsers;
+    for(User user : users){
+        if(user.state == state){
+            matchedUsers.push_back(user.displayName);
+        }
+    }
+    return matchedUsers;
+}
+
+int main(){
+    string userFilePath, postFilePath;
+    int choice = 0;
+    bool loop = true;
+    string userFile, postFile, postId, username, state;
+    cout << "Please Select a Numbered Option From the Following:\n";
+    cout << "1. Load input data\n";
+    cout << "2. Check visibility\n";
+    cout << "3. Retrieve posts\n";
+    cout << "4. Search users by location\n";
+    cout << "5. Exit\n";
+    cin >> choice;
+    while(loop){
+        cout << "Please Select a Numbered Option From the Following:\n";
+        cout << "1. Load input data\n";
+        cout << "2. Check visibility\n";
+        cout << "3. Retrieve posts\n";
+        cout << "4. Search users by location\n";
+        cout << "5. Exit\n";
+        cin >> choice;
+        if(choice == 1){
+            cout << "Enter user file path: ";
+            cin >> userFile;
+            cout << "Enter post file path: ";
+            cin >> postFile;
+            loadUserData(userFile);
+            loadPostData(postFile);
+            cout << "Data from user and post successfully loaded!\n";
+        }else if(choice == 2){
+            cout << "Enter a postID: ";
+            cin >> postId;
+            cout << "Enter the username: ";
+            cin >> username;
+            cout << checkVisibility(postId, username);
+        }else if(choice == 3){
+            cout << "Enter a username: ";
+            cin >> username;
+            for(string postId : retrievePosts(username)){
+                cout << postId << endl;
+            }
+        }else if(choice == 4){
+            cout << "Enter user state location (state initials): ";
+            cin >> state; 
+            for(string displayName : searchUsersByLocation(state)){
+                cout << displayName << endl;
+            }
+        }else if(choice == 5){
+            cout << "Exiting Program." << endl;
+            loop = false;
+        }else{
+          cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+    return 0;
+    
+}
